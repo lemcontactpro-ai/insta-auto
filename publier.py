3937f3dd3a7d38f0a1797928e4330f7a5507118b
lemcontactpro-ai <lemcontactpro@gmail.com>
2026-09-10 23:14:04 +0200

    Update publier.py

--- publier.py
+++ publier.py
@@ -191,8 +191,9 @@ def heberger(chemin):
         str(chemin),
         folder="insta-auto",
         public_id=f"{chemin.stem}_{datetime.now():%Y%m%d%H%M%S}",
+        format="jpg",
+        resource_type="image",
     )
-    return res["secure_url"]
 
 
 # --------------------------------------------------------------- instagram
